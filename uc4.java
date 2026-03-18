c52f925d42592e8522cae31357a53d7b39642b29
Sautrik <sr9973@srmist.edu.in>
2026-03-18 11:03:52 +0530

    UC4 done

---
+++ uc4.java
@@ -0,0 +1,13 @@
+public class uc4 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World");
+            return;
+        }
+      
+        for (String name : args) {
+            System.out.println("Hello, " + name);
+        }
+    }
+}
