c6e9236ae283247b2c676c5cb7ec38651838a6f3
Sautrik <sr9973@srmist.edu.in>
2026-04-02 06:57:29 +0530

    New file

---
+++ uc6.java
@@ -0,0 +1,18 @@
+public class uc6 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        String result = "";
+
+        for (String name : args) {
+            result += name + ", ";
+        }
+
+        result = result.substring(0, result.length() - 2);
+
+        System.out.println("Hello, " + result + "!");
+    }
+}
